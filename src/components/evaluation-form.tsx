22f0aeee130077a2c235bacbdbef82cd7cc02bc6
Verathagnus <59093644+Verathagnus@users.noreply.github.com>
2023-07-13 12:49:00 +0530

    feat: loading mdal

--- src/components/evaluation-form.tsx
+++ src/components/evaluation-form.tsx
@@ -21,6 +21,7 @@ export default function EvaluationForm() {
   const [showModal, setShowModal] = useState(false)
   const [isOpen, setIsOpen] = useState(false)
   const [fileContent, setFileContent] = useState('')
+  const [loading, setLoading] = useState(false)
   const openModal = (e: React.ChangeEvent<HTMLFormElement>) => {
     e.preventDefault()
     setIsOpen(true)
@@ -144,10 +145,20 @@ export default function EvaluationForm() {
     e: React.ChangeEvent<HTMLFormElement>
   ) => {
     e.preventDefault()
+    setLoading(true)
+    setSubmitMessage(``)
+
     // console.log(teamName, selectedSubmissionType["name"], selectedTranslationDirection["name"]);
     if (selectedFiles.length === 0) {
-      setSubmitMessage(`Please upload a file`)
+      setSubmitMessage(`Please upload a text file`)
       scrollToBottom()
+      setLoading(false)
+      return
+    }
+    if (selectedFiles[0].type != 'text/plain') {
+      setSubmitMessage(`Please upload a text file`)
+      scrollToBottom()
+      setLoading(false)
       return
     }
     console.log('Selected File:', selectedFiles)
@@ -201,29 +212,54 @@ export default function EvaluationForm() {
         setResult(`Check number of lines of file uploaded`)
         scrollToBottom()
       }
+    } finally {
+      setLoading(false)
     }
   }
 
   const handleSubmitResult = async (e: any) => {
     // e.preventDefault()
     closeModal()
+    setSubmitMessage(``)
+
+    setLoading(true)
     // console.log(teamName, selectedSubmissionType["name"], selectedTranslationDirection["name"]);
     if (selectedFiles.length === 0) {
-      setSubmitMessage(`Please upload a file`)
+      setSubmitMessage(`Please upload a text file for output`)
       scrollToBottom()
+      setLoading(false)
       return
     }
+    if (selectedFiles[0].type != 'text/plain') {
+        setSubmitMessage(`Please upload a text file for output`)
+        scrollToBottom()
+        setLoading(false)
+        return
+      }
     if (systemDescription.length === 0) {
-      setSubmitMessage(`Please upload a file`)
+      setSubmitMessage(`Please upload a pdf file for system description`)
       scrollToBottom()
+      setLoading(false)
       return
     }
     if (
       systemDescription.length > 0 &&
-      systemDescription[0].type === 'application/pdf'
+      systemDescription[0].type !== 'application/pdf'
     ) {
       setSubmitMessage(`Please upload a pdf file for system description`)
       scrollToBottom()
+      setLoading(false)
+      return
+    }
+
+    const MAX_FILE_SIZE = 10 * 1024 * 1024 // 10 MB in bytes\
+    if (
+      systemDescription.length > 0 &&
+      systemDescription[0].size > MAX_FILE_SIZE
+    ) {
+      setSubmitMessage(`Please upload a pdf file less than 10 MB size`)
+      scrollToBottom()
+      setLoading(false)
       return
     }
 
@@ -284,6 +320,8 @@ export default function EvaluationForm() {
       if (err?.response?.status === 400) {
         setSubmitMessage(err?.response?.data?.message)
       }
+    } finally {
+      setLoading(false)
     }
   }
 
@@ -356,6 +394,54 @@ export default function EvaluationForm() {
               </div>
             </Transition.Child>
           </div>
+        </Dialog>
+      </Transition.Root>
+      <Transition.Root show={loading} as={React.Fragment}>
+        <Dialog
+          as="div"
+          className="fixed inset-0 z-10 flex items-center justify-center"
+          onClose={() => {}}
+        >
+          <Transition.Child
+            as={React.Fragment}
+            enter="transition-opacity duration-300"
+            enterFrom="opacity-0"
+            enterTo="opacity-100"
+            leave="transition-opacity duration-300"
+            leaveFrom="opacity-100"
+            leaveTo="opacity-0"
+          >
+            <Dialog.Overlay className="fixed inset-0 bg-black opacity-60" />
+          </Transition.Child>
+          <Transition.Child
+            as={React.Fragment}
+            enter="transition-opacity duration-300"
+            enterFrom="opacity-0"
+            enterTo="opacity-100"
+            leave="transition-opacity duration-300"
+            leaveFrom="opacity-100"
+            leaveTo="opacity-0"
+          >
+            <div className="flex items-center justify-center">
+              {/* Replace the following with your SVG component */}
+              <svg
+                aria-hidden="true"
+                className="mr-2 h-8 w-8 animate-spin fill-purple-600 text-gray-200 dark:text-gray-600"
+                viewBox="0 0 100 101"
+                fill="none"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <path
+                  d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z"
+                  fill="currentColor"
+                />
+                <path
+                  d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z"
+                  fill="currentFill"
+                />
+              </svg>
+            </div>
+          </Transition.Child>
         </Dialog>
       </Transition.Root>
       <div className="mt-5 flex min-h-full flex-1 flex-col justify-center  px-6 sm:mx-auto sm:w-full sm:max-w-[50rem] lg:px-8">
